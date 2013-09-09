406626321fe6339cd4f75e0a06673194d690b536
RyanBurnside <pixeloutlaw@gmail.com>
2013-09-08 21:18:24 -0600

    Added a standard for drawing codes.

--- Parser/parser.h
+++ Parser/parser.h
@@ -30,8 +30,8 @@ public:
   void reset()
   {
     // Reset the search index and set it possible once more
-    this->still_possible = true;
-    this->search_index = 0;
+    still_possible = true;
+    search_index = 0;
   }
 
   bool still_valid()
@@ -48,7 +48,7 @@ public:
     if(still_possible == true)
     {
       // Tests to see if index is still valid
-      if (search_index == code.length())
+      if (search_index >= code.length())
       {
 	// The word has failed, return false
 	still_possible = false;
@@ -64,7 +64,11 @@ public:
 	// Character matches final char and code matches!
 	if(search_index == code.length() -1)
 	{
+	  std::cout << "FOUND ONE!" << "Code is: " << get_code()
+	            << " Description is: " << get_desc() << std::endl;
+	  // Now reset the state
 	  still_possible = false;
+	  search_index = 0;
 	  return true;
 	}
 	// The character matches but the search is not yet complete, iterate
@@ -78,10 +82,9 @@ public:
 	return false;
       }
     }
-    else
-    {
-      return false;
-    }
+
+    return false;
+
   }
 
 private:
@@ -135,7 +138,7 @@ void set_codes()
 
   codes.push_back(Chr_code("[=", "Set alternate keypad mode"));
   codes.push_back(Chr_code("[>", "Set numeric keypad mode"));
-
+  
   codes.push_back(Chr_code("[(A", "Set United Kingdom G0 character set"));
   codes.push_back(Chr_code("[)A", "Set United Kingdom G1 character set"));
   codes.push_back(Chr_code("[(B", "Set United States G0 character set"));
@@ -146,10 +149,10 @@ void set_codes()
   codes.push_back(Chr_code("[)1", "Set G1 alternate character ROM"));
   codes.push_back(Chr_code("[(2", "Set G0 alt char ROM and spec."));
   codes.push_back(Chr_code("[)2", "Set G1 alt char ROM and spec.")); 
-
+  
   codes.push_back(Chr_code("[N", "Set single shift 2"));
   codes.push_back(Chr_code("[O", "Set single shift 3"));
-
+  
   codes.push_back(Chr_code("[m", "Turn off character attributes"));
   codes.push_back(Chr_code("[0m", "Turn off character attributes"));
   codes.push_back(Chr_code("[1m", "Turn bold mode on"));
@@ -158,10 +161,10 @@ void set_codes()
   codes.push_back(Chr_code("[5m", "Turn blinking mode on"));
   codes.push_back(Chr_code("[7m", "Turn reverse video on"));
   codes.push_back(Chr_code("[8m", "Turn invisible text mode on"));
-
+  
   codes.push_back(Chr_code("[<v>;<v>r", 
-			   "Set top and bottom line #s of a window", true));
-
+  			   "Set top and bottom line #s of a window", true));
+  
   codes.push_back(Chr_code("[<n>A", "Move cursor up n lines", true));
   codes.push_back(Chr_code("[<n>B", "Move cursor down n lines", true));
   codes.push_back(Chr_code("[<n>C", "Move cursor right n lines", true));
@@ -169,87 +172,63 @@ void set_codes()
   codes.push_back(Chr_code("[H", "Move cursor to upper left corner"));
   codes.push_back(Chr_code("[;H", "Move cursor to upper left corner"));
   codes.push_back(Chr_code("[<v>;<h>H", "Move cursor to screen location v,h",
-			   true));
+  			   true));
   codes.push_back(Chr_code("[f", "Move cursor to upper left corner"));
   codes.push_back(Chr_code("[;f", "Move cursor to upper left corner"));
   codes.push_back(Chr_code("[<v>;<h>f", "Move cursor to screen location v,h",
-			   true));
-
+  			   true));
+  
   codes.push_back(Chr_code("[D", "Move/scroll window up one line"));
   codes.push_back(Chr_code("[M", "Move/scroll window down one line"));
   codes.push_back(Chr_code("[E", "Move to next line"));
   codes.push_back(Chr_code("[7", "Save cursor position and attributes"));
   codes.push_back(Chr_code("[8", "Restore cursor position and attributes"));
-
+  
   codes.push_back(Chr_code("[H", "Set a tab at the current column"));
   codes.push_back(Chr_code("[g", "Clear a tab at the current column"));
   codes.push_back(Chr_code("[0g", "Clear a tab at the current column"));
   codes.push_back(Chr_code("[3g", "Clear all tabs"));
-
+  
   codes.push_back(Chr_code("[#3", "Double-height letters, top half"));
   codes.push_back(Chr_code("[#4", "Double-height letters, bottom half"));
   codes.push_back(Chr_code("[#5", "Single width, single height letters"));
   codes.push_back(Chr_code("[#6", "Double width, single height letters"));
-
+  
   codes.push_back(Chr_code("[K", "Clear line from cursor right"));
   codes.push_back(Chr_code("[0K", "Clear line from cursor right"));
   codes.push_back(Chr_code("[1K", "Clear line from cursor left"));
   codes.push_back(Chr_code("[2K", "Clear entire line"));
-
+  
   codes.push_back(Chr_code("[J", "Clear screen from cursor down"));
   codes.push_back(Chr_code("[0J", "Clear screen from cursor down"));
   codes.push_back(Chr_code("[1J", "Clear screen from cursor up"));
   codes.push_back(Chr_code("[2J", "Clear entire screen"));
-
+  
   codes.push_back(Chr_code("[5n", "Device status report"));
   codes.push_back(Chr_code("[0n", "Response: terminal is OK"));
   codes.push_back(Chr_code("[3n", "Response: terminal is not OK"));
-
+  
   codes.push_back(Chr_code("[6n", "Get cursor position"));
   codes.push_back(Chr_code("[<v>;<h>R", "Response: cursor is at v,h", true));
-
+  
   codes.push_back(Chr_code("[c", "Identify what terminal type"));
   codes.push_back(Chr_code("[0c", "Identify what terminal type (another)"));
   codes.push_back(Chr_code("[?1;<n>0c", "Response: terminal type code n", 
-			   true));
-
+  			   true));
+  
   codes.push_back(Chr_code("[c", "Reset terminal to initial state"));
-
+  
   codes.push_back(Chr_code("[#8", "Screen alignment display"));
   codes.push_back(Chr_code("[2;1y", "Confidence power up test"));
   codes.push_back(Chr_code("[2;2y", "Confidence loopback test"));
   codes.push_back(Chr_code("[2;9y", "Repeat power up test"));
   codes.push_back(Chr_code("[2;10y", "Repeat loopback test"));
-
+  
   codes.push_back(Chr_code("[0q", "Turn off all four leds"));
   codes.push_back(Chr_code("[1q", "Turn on LED #1"));
   codes.push_back(Chr_code("[2q", "Turn on LED #2"));
   codes.push_back(Chr_code("[3q", "Turn on LED #3"));
   codes.push_back(Chr_code("[4q", "Turn on LED #4"));
-
-  // All codes below are for use in VT52 compatibility mode.
-
-  codes.push_back(Chr_code("[<", "Enter/exit ANSI mode (VT52)"));
-
-  codes.push_back(Chr_code("[=", "Enter alternate keypad mode"));
-  codes.push_back(Chr_code("[>", "Exit alternate keypad mode"));
-
-  codes.push_back(Chr_code("[F", "Use special graphics character set"));
-  codes.push_back(Chr_code("[G", "Use normal US/UK character set"));
-
-  codes.push_back(Chr_code("[A", "Move cursor up one line"));
-  codes.push_back(Chr_code("[B", "Move cursor down one line"));
-  codes.push_back(Chr_code("[C", "Move cursor right one char"));
-  codes.push_back(Chr_code("[D", "Move cursor left one char"));
-  codes.push_back(Chr_code("[H", "Move cursor to upper left corner"));
-  codes.push_back(Chr_code("[<v><h>", "Move cursor to v,h location", true));
-  codes.push_back(Chr_code("[I", "Generate a reverse line-feed"));
-
-  codes.push_back(Chr_code("[K", "Erase to end of current line"));
-  codes.push_back(Chr_code("[J", "Erase to end of screen"));
-
-  codes.push_back(Chr_code("[Z", "Identify what the terminal is"));
-  codes.push_back(Chr_code("[/Z", "Correct response to ident"));
 }
 
 void scan_letter(char c)
@@ -279,9 +258,6 @@ void scan_letter(char c)
       {
 	if(i->now_matches(c))
 	{
-	  std::cout << "FOUND ONE!" << "Code is: " << i->get_code()
-	            << " Description is: " << i->get_desc() << std::endl;
-	  // Now reset the state
 	  pattern_compleation_mode = false;
 	  reset_codes();
 	}
--- Parser/parser_stub_test.cpp
+++ Parser/parser_stub_test.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-#include<vector>
-#include<string>
+#include <vector>
+#include <string>
 #include "parser.h"
 
 int main(int argc, char* argv[])
@@ -11,16 +11,15 @@ int main(int argc, char* argv[])
   // now create a real test string
   for(int i = 0; i < codes.size(); ++i)
   {
-    test_string += char(27);
-    test_string += (codes[i].get_code());
+    test_string.append("\33");
+    test_string.append(codes[i].get_code());
   }
-
-  std::cout << "Currently, many codes are skipped, no idea why" << std::endl;
-
+  std::cout << "If you can see this you need to be reading it from a hex" <<
+    "editor and not a terminal, ensure that no NULL \0 or odd characters" <<
+    "are being written to the string!" << std::endl;
   for(int i = 0; i < test_string.length(); ++i)
   {
     scan_letter(test_string[i]);
   }
-  
   return 0;
 }
